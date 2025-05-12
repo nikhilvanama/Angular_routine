ec2b52d1c2ed3669ed487e58c690bfbd91339c3c
Nikhil vanama <vanamanikhil15@gmail.com>
2025-05-12 18:53:52 +0530

    learning angular

--- angular1/src/app/app.component.ts
+++ angular1/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterAppComponent } from "./footer-app/footer-app.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterAppComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular1';
---
+++ angular1/src/app/footer-app/footer-app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-app',
+  imports: [],
+  template: `
+  <h1>Footer</h1>
+  `,
+  styleUrl: './footer-app.component.css'
+})
+export class FooterAppComponent {
+
+} 
