62a26e6e1d4bbe99816518214cdbd47848ee2ebd
Nikhil vanama <vanamanikhil15@gmail.com>
2025-05-13 18:11:44 +0530

    Completed Two-way binding

--- angular1/src/app/app.component.ts
+++ angular1/src/app/app.component.ts
@@ -1,26 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterAppComponent } from "./footer-app/footer-app.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterAppComponent],
+  imports: [RouterOutlet, FooterAppComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular19';
 
-  vcol:string = 'brown';
-  red:string = 'red';
+  counter: number = 0;
+  increment() {
+    this.counter++;
+  }
 
-  gcol:object = {
-    'background-color': 'green',
-    'color': 'white',
-    'font-size': '20px',
-    'font-weight': 'bold',
-    'padding': '10px'
-  };
+  decrement() {
+    this.counter--;
+  }
 
-  isactive: boolean = false;
+  name:string = "";
+
+  changeName(e:any){
+    console.log(e);
+    console.log(e.target.value);
+    this.name = e.target.value;
+  }
+
+  city: string = "Hyderabad";
+
 }
