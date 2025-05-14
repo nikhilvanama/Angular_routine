a2e76d4e4cd1537e256c6af13733b715f05cf2f1
Nikhil vanama <vanamanikhil15@gmail.com>
2025-05-14 23:20:22 +0530

    Derivates and pipes examples

--- angular1/src/app/app.component.ts
+++ angular1/src/app/app.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterAppComponent } from "./footer-app/footer-app.component";
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterAppComponent, FormsModule],
+  imports: [RouterOutlet, FooterAppComponent, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular19';
+  title = 'Angular19';
 
   counter: number = 0;
   increment() {
@@ -19,6 +20,10 @@ export class AppComponent {
 
   decrement() {
     this.counter--;
+  }
+
+  reset() {
+    this.counter = 0;
   }
 
   name:string = "";
@@ -31,4 +36,36 @@ export class AppComponent {
 
   city: string = "Hyderabad";
 
+  name_ng:string = "";
+
+  isTrue = "5";
+
+  status = "online";
+
+  items = [
+    { name: "text", id: 1 },
+    { name: "number", id: 2 },
+    { name: "number", id: 3 },
+    { name: "text", id: 4 },
+    { name: "email", id: 5 }];
+
+  names = [
+    { name: "Kali", id: 1 },
+    { name: "Nikhil", id: 2 },
+    { name: "Krishna", id: 3 },
+    { name: "Nick", id: 4 },
+    { name: "Sita", id: 5 }
+  ];
+
+  symbol: string = "";
+  num1:number = 4;
+  num2:number = 5;
+
+  today:string = new Date().toLocaleDateString();
+
+  man = {
+    name: "Kali",
+    age: 25,
+    city: "Hyderabad"
+  };
 }
