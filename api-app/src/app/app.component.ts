eb8b4dda8b8d8a00adf4a5041e337ddf79afc6c5
Nikhil vanama <vanamanikhil15@gmail.com>
2025-05-22 16:25:11 +0530

    Completed getting my first API Response

--- api-app/src/app/app.component.ts
+++ api-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsService } from './services/products.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'api-app';
+
+  constructor(private productser:ProductsService){}
+
+  productList:any;
+
+  ngOnInit(){
+    this.productser.getProductList().subscribe((data:any)=>{
+      console.log(data);
+
+      this.productList=data.products;
+    })
+  }
 }
