bed3c16902421bb40cc96330a8688a31cb0b0984
Nikhil vanama <vanamanikhil15@gmail.com>
2025-05-22 22:20:29 +0530

    Completed Lazy loading concept

--- route-app/src/app/app.routes.ts
+++ route-app/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
+// import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
@@ -9,7 +9,8 @@ import { UserComponent } from './user/user.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
+    // { path: 'about', component: AboutComponent },
+    {path:'about', loadComponent:()=>import('./about/about.component').then((c)=>c.AboutComponent)},
     { path: 'contact', component: ContactComponent },
     { path: 'login', component: LoginComponent },
     { path: 'user/:id/:name', component: UserComponent },
