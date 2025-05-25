d39a1e4e7d5a9ad0a759a0d4069c43f37bbc20cc
Nikhil vanama <vanamanikhil15@gmail.com>
2025-05-25 11:56:56 +0530

    Completed Task Manager Project, Button activity is pending

--- task-manager/src/app/app.component.ts
+++ task-manager/src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component,computed,inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskService } from './services/task.service';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TaskFormComponent, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'task-manager';
+  
+ taskService = inject(TaskService);
+
+ tasks = this.taskService.tasks;
+
+ todoItems = computed(()=> {
+  const tasks = this.tasks();
+  return tasks.filter(task => task.status === 'todo');
+ })
+
+  inProgress = computed(()=> {
+  const tasks = this.tasks();
+  return tasks.filter(task => task.status === 'inprogress');
+ })
+
+  Completed = computed(()=> {
+  const tasks = this.tasks();
+  return tasks.filter(task => task.status === 'completed');
+ })
+
 }
---
+++ task-manager/src/app/components/task-form/task-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { TaskService } from '../../services/task.service';
+
+
+@Component({
+  selector: 'app-task-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './task-form.component.html',
+  styleUrl: './task-form.component.css'
+})
+export class TaskFormComponent {
+
+  taskService = inject(TaskService);
+
+  taskForm = new FormGroup({
+    task: new FormControl('', {
+      nonNullable: true
+    }),
+    status: new FormControl('todo', {
+      nonNullable: true
+    })
+  })
+
+  onSubmit() {
+    console.log(this.taskForm.value);
+    // Here you would typically send the form data to a server or handle it as needed
+
+    const taskValue = this.taskForm.getRawValue();
+    this.taskService.addTask(taskValue.task, taskValue.status); // Use the service to add the task
+
+    console.log('Task added:', taskValue);
+    this.taskForm.reset(); // Reset the form after submission
+  }
+}
