7e5a84f222530e5b9e4732cc8689d1c791611b05
Nikhil vanama <vanamanikhil15@gmail.com>
2025-06-06 16:18:23 +0530

    Added Forms Module and Added Form Group to form and logged data after success Submission

--- crud-op/src/app/features/post/pages/create-post/create-post.component.ts
+++ crud-op/src/app/features/post/pages/create-post/create-post.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-post',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-post.component.html',
   styleUrl: './create-post.component.css'
 })
 export class CreatePostComponent {
+
+  CreatePostForm = new FormGroup({
+    title: new FormControl<string>('', [Validators.required, Validators.minLength(3)]),
+    description: new FormControl<string>('', [Validators.required, Validators.minLength(10)]),
+    image: new FormControl('', [Validators.required]),
+  });
+
+  onSubmit() {
+    const postValue = this.CreatePostForm.getRawValue();
+    console.log('Post Value:', postValue);
+    console.log(this.CreatePostForm.value);
+  } 
 
 }
