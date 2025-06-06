7131cbddb25ef433221c3d331befd38d4d351b24
Nikhil vanama <vanamanikhil15@gmail.com>
2025-06-06 12:54:01 +0530

    Added Navbar, Create Post Component

--- crud-op/src/app/app.routes.ts
+++ crud-op/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CreatePostComponent } from './features/post/pages/create-post/create-post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent },         // Default route
+    { path: 'createpost', component: CreatePostComponent }, 
+    { path: '**', redirectTo: '' }  // Wildcard route to handle 404
+];
