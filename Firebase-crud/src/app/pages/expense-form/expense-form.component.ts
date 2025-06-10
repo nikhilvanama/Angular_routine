6fb73d21e7179a62114f0b76838dcdcda25bf0ec
Nikhil vanama <vanamanikhil15@gmail.com>
2025-06-10 09:54:28 +0530

    Created forms, cards, routing for angular CRUD operation Application

--- Firebase-crud/src/app/pages/expense-form/expense-form.component.ts
+++ Firebase-crud/src/app/pages/expense-form/expense-form.component.ts
@@ -1,11 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-expense-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './expense-form.component.html',
   styleUrl: './expense-form.component.css'
 })
 export class ExpenseFormComponent {
 
+  expenseForm = new FormGroup({
+    price: new FormControl<string>('', [Validators.required, Validators.minLength(1)]),
+    title: new FormControl<string>('', [Validators.required, Validators.minLength(5)]),
+    description: new FormControl<string>('', [Validators.required, Validators.minLength(10), Validators.maxLength(500)]),
+  });
+
+  get price() {
+    return this.expenseForm.get('price');
+  }
+
+  get title() {
+    return this.expenseForm.get('title');
+  }
+
+  onSubmit() {
+    if (this.expenseForm.invalid) {
+      this.expenseForm.markAllAsTouched(); // show validation messages
+      return;
+    }
+
+    const postValue = this.expenseForm.getRawValue();
+    console.log('Expense Values:', postValue);
+
+    // Reset Form
+    this.expenseForm.reset();
+  }
 }
--- Firebase-crud/src/app/pages/expense/expense.component.ts
+++ Firebase-crud/src/app/pages/expense/expense.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-expense',
-  imports: [],
+  imports: [CommonModule, RouterModule, RouterLink, RouterOutlet],
   templateUrl: './expense.component.html',
   styleUrl: './expense.component.css'
 })
